ad831f420ee2fdd9355d75d6c80afddb60fc815b
holyshared <holy.shared.design@gmail.com>
2017-03-11 18:44:06 +0900

    add report texts

--- lib/i18n_checker/rake_task.rb
+++ lib/i18n_checker/rake_task.rb
@@ -10,11 +10,16 @@ module I18nChecker
     attr_accessor :name
     attr_accessor :locale_file_paths
     attr_accessor :haml_template_paths
+    attr_accessor :logger
 
     def initialize(name = :locale_check)
       @name = name
       @haml_template_paths = FileList['./app/views/*']
       @locale_file_paths = FileList['./locales/*']
+      @logger = Logger.new(STDOUT)
+      @logger.formatter = proc {|severity, datetime, progname, message|
+        "#{message}\n"
+      }
       yield self if block_given?
       define
     end
@@ -27,7 +32,7 @@ module I18nChecker
     end
 
     def run_task
-      reporter = I18nChecker::Reporter::DefaultReporter.new
+      reporter = I18nChecker::Reporter::DefaultReporter.new(logger: logger)
       locale_files = I18nChecker::Locale.load_of(locale_file_paths)
       locale_texts = I18nChecker::Haml.collect_locale_text_of(haml_template_paths)
 
--- lib/i18n_checker/reporter/default_reporter.rb
+++ lib/i18n_checker/reporter/default_reporter.rb
@@ -4,9 +4,10 @@ module I18nChecker
   module Reporter
     class DefaultReporter < DetectResultReporter
       def report(result)
+        logger.info 'There are settings where translated text can not be found'.red
         result.locale_texts.each do |locale_text|
-          logger.info locale_text.file_name.cyan
-          logger.info "  line: #{locale_text.line} - #{locale_text.lang}.#{locale_text.text}"
+          logger.info "  #{locale_text.file_name.cyan}"
+          logger.info "    line: #{locale_text.line} - #{locale_text.lang}.#{locale_text.text}"
         end
       end
     end
